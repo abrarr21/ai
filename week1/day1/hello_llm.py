73b40ce2388c0d63df7621ff43c0aac6fb665940
abrarr21 <abrarrr2110@gmail.com>
2026-07-07 14:12:22 +0530

    llm call

---
+++ week1/day1/hello_llm.py
@@ -0,0 +1,27 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+my_api_key = os.getenv("GROQ_API_KEY")
+
+if not my_api_key:
+    raise ValueError("API key kaha love_de")
+
+client = Groq(api_key=my_api_key)
+
+model = "llama-3.3-70b-versatile"
+role = "user"
+prompt = "Who was the first prime minister of India"
+message = {"role": role, "content": prompt}
+
+messages = [message]
+
+response = client.chat.completions.create(model=model, messages=messages)
+print(response)
+
+print("#################################")
+
+answer = response.choices[0].message.content
+print(answer)
